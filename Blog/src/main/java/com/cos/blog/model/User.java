0928ceb61dc9bc87be191ed08f46e41c3ec58419
Maberick1110 <80397195+Maberick1110@users.noreply.github.com>
2021-11-24 20:01:24 +0900

    last
    last

---
+++ Blog/src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,53 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.description.modifier.EnumerationState;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//@DynamicInsert//
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false,length = 30, unique=true)
+	private String username;
+	
+	@Column(nullable = false,length = 100)
+	private String password;
+	
+	@Column(nullable = false,length = 50)
+	private String email;
+	
+	//@ColumnDefault("User")//
+	@Enumerated(EnumType.STRING)
+	private RoleType role;
+	
+	@CreationTimestamp
+	private Timestamp cratedate;
+	
+}
